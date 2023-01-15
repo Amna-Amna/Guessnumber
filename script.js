4ecd70ec88e460d47245f0377792f6e91aecba37
Amna-Amna <mailtoamna4@gmail.com>
2023-01-15 16:50:24 +0500

    Added Highscore fuctionality

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.highscore').textContent = score;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '💹 Too high';
